8bc4546fe04a0119310327e982b720c3a55c2e81
Nomta <nomta@outlook.com>
2023-07-05 13:17:33 +0300

    ui paination

---
+++ src/components/ui/UiPagination.vue
@@ -0,0 +1,23 @@
+<template>
+  <ElPagination v-model:current-page="currentPage" :page-size="itemCount" :pager-count="linkCount" layout="prev, pager, next" :total="totalCount" background />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  modelValue: number
+  itemCount?: number
+  linkCount?: number
+  totalCount: number
+}>()
+
+const emit = defineEmits<{
+  'update:modelValue': [newValue: number]
+}>()
+
+const currentPage = computed({
+  get: () => props.modelValue,
+  set: (newValue) => emit('update:modelValue', newValue),
+})
+</script>
