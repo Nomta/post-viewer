ad74ecf03639899bb916ea24f621f69c4add8566
Nomta <nomta@outlook.com>
2023-07-10 01:52:20 +0300

    pagination

---
+++ src/components/PostPagination.vue
@@ -0,0 +1,26 @@
+<template>
+  <UiPagination  
+    v-model="currentPage" 
+    :item-count="countPerPage" 
+    :total-count="totalCount" 
+    class="post-pagination" 
+  />
+</template>
+
+<script setup lang="ts">
+const currentPage = defineModel()
+
+defineProps<{
+  totalCount: number
+}>()
+
+const countPerPage = 10
+
+</script>
+
+<style scoped>
+.post-pagination {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+</style>
